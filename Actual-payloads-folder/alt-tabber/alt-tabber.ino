ff9708f3f084ab78fee13d598c3c0ef0f2072997
Le Dique <59876805+Blubberdique@users.noreply.github.com>
2022-07-05 14:58:33 +0200

    Included alt-tab payload

---
+++ Actual-payloads-folder/alt-tabber/alt-tabber.ino
@@ -0,0 +1,22 @@
+// Test file for alt-tabbing
+
+#include "DigiKeyboard.h"
+#define KEY_TAB 0x2B
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  DigiKeyboard.update();
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(3000);
+
+  DigiKeyboard.sendKeyStroke(MOD_ALT_LEFT , KEY_TAB);
+  DigiKeyboard.sendKeyStroke(MOD_ALT_LEFT , KEY_TAB);
+  DigiKeyboard.sendKeyStroke(MOD_ALT_LEFT , KEY_TAB);
+  DigiKeyboard.sendKeyStroke(MOD_ALT_LEFT , KEY_TAB);
+  DigiKeyboard.sendKeyStroke(MOD_ALT_LEFT , KEY_TAB);
